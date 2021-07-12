5a6b32f0d7407edd36247c70093898ef733616f5
swapsCAPS <verstegen.daan@gmail.com>
2021-07-12 23:26:53 +0200

    working simple template matching with alpha channel
    Matching on scanned images will not be as easy as this. And this was not
    easy... :tada:

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-
+import cv2
 
 def zoom_to_bounds(image_arr):
     mask = image_arr > 0
@@ -21,3 +21,30 @@ def zoom_to_bounds(image_arr):
         new_image, 28, 28
     )
     return new_image
+
+# Expects image and template both with alpha channel
+def simpleMatchTemplate(img, tmpl, threshold=1, norm=False):    
+    img = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+    tmpl = cv2.imread(tmpl, cv2.IMREAD_UNCHANGED)
+
+    if img.shape[2] != 4:
+        raise Exception("Image needs alpha channel")
+    if tmpl.shape[2] != 4:
+        raise Exception("Template needs alpha channel")
+        
+    alpha_mask = np.array(cv2.split(tmpl)[3])
+    
+    res = cv2.matchTemplate(img, tmpl, cv2.TM_CCORR_NORMED, mask=alpha_mask)
+    # Normalize from 0 to 1
+    if norm:
+        res = cv2.normalize(res, None, 0, 1, cv2.NORM_MINMAX, -1)
+    
+    match_locations = np.where(res >= threshold)
+    
+    h, w = tmpl.shape[:2]
+    
+    r = []
+    for (x, y) in zip(*match_locations[::-1]):
+        r.append(((x, y), (x + w, y + h)))
+        
+    return r

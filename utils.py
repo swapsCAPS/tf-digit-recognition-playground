dab405472f36c2d713eec5f6bd01654da2008d1b
swapsCAPS <verstegen.daan@gmail.com>
2021-07-24 14:55:05 +0200

    WIP findHomography

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -5,7 +6,22 @@ import math
 from typing import Tuple, Union
 from deskew import determine_skew
 
-def deskew(image: np.ndarray, blur: int = 13, threshold: int = 250, debug=False):
+# Takes a grayscale image
+def black_blur(image: np.ndarray, blur: int = 13, threshold: int = 250):
+    # TODO what if it is gray already? or other shape?
+    print(image.shape)
+
+    res = None
+
+    if len(image.shape) > 2:
+        res = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    th, res = cv2.threshold(res, threshold, 255, cv2.THRESH_BINARY)
+    if blur > 0:
+        res = cv2.GaussianBlur(res, (blur,blur), 0)
+    return res
+
+def deskew(image: np.ndarray, blur: int = 13, threshold: int = 250, debug=False, do_blur=True):
     def rotate(
             image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]]
     ) -> np.ndarray:
@@ -19,34 +35,36 @@ def deskew(image: np.ndarray, blur: int = 13, threshold: int = 250, debug=False)
         rot_mat[1, 2] += (width - old_width) / 2
         rot_mat[0, 2] += (height - old_height) / 2
         return cv2.warpAffine(image, rot_mat, (int(round(height)), int(round(width))), borderValue=background)
-    
+
     image_copy = image.copy()
-    
-    grayscale = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
-    th, thresh = cv2.threshold(grayscale, threshold, 255, cv2.THRESH_BINARY)
-    blurred = cv2.GaussianBlur(thresh, (blur,blur), 0)
-    
-    angle = determine_skew(blurred)
+
+    if do_blur:
+        image_copy = black_blur(image_copy, blur, threshold)
+
+    angle = determine_skew(image_copy)
 
     if (debug is True): print(f"deskew() Found angle: {angle}")
-    
-    if (angle < -75): angle = angle + 90
 
-    rotated = rotate(image_copy, angle, (255, 255, 255))
-    return rotated
+    if (angle is not None):
+        if (angle < -75): angle = angle + 90
+        image_copy = rotate(image_copy, angle, (255, 255, 255))
 
-def zoom_to_bounds(image_arr):
+    return image_copy
+
+def crop_to_content(image_arr):
     mask = image_arr > 0
     coords = np.argwhere(mask)
-    
+
     tl = (coords.min(axis=0)[0:2])
     br = (coords.max(axis=0) + 1)[0:2]
     height = br[0] - tl[0]
     width = br[1] - tl[1]
 
-    new_image = tf.image.crop_to_bounding_box(
-        image_arr, tl[0], tl[1], height, width
-    )
+    return image_arr[tl[0]:tl[0] + height, tl[1]:tl[1] + width]
+
+def zoom_to_bounds(image_arr):
+    new_image = crop_to_content(image_arr)
+
     new_image = tf.image.resize(
         new_image, (28,28), method=tf.image.ResizeMethod.BILINEAR, preserve_aspect_ratio=True
     )
@@ -56,28 +74,67 @@ def zoom_to_bounds(image_arr):
     return new_image
 
 # Expects image and template both with alpha channel
-def simpleMatchTemplate(img, tmpl, threshold=1, norm=False):    
-    img = cv2.imread(img, cv2.IMREAD_UNCHANGED)
-    tmpl = cv2.imread(tmpl, cv2.IMREAD_UNCHANGED)
+def simpleMatchTemplate(
+    img: np.ndarray,
+    tmpl: np.ndarray,
+    threshold=1,
+    norm=False,
+    debug=False,
+    rect_thresh = 1.5,
+):
+    def print_debug(*args):
+        if (not debug): return
+        print("simpleMatchTemplate()", *args)
+
+    if img is None:
+        raise Exception("Please provide an image")
+
+    if tmpl is None:
+        raise Exception("Please provide an template")
+
+    print_debug(f"img.shape:        {img.shape}")
+    print_debug(f"tmpl.shape:       {tmpl.shape}")
 
     if img.shape[2] != 4:
         raise Exception("Image needs alpha channel")
     if tmpl.shape[2] != 4:
         raise Exception("Template needs alpha channel")
-        
+
     alpha_mask = np.array(cv2.split(tmpl)[3])
-    
+    print('alpha_mask', alpha_mask)
+    cropped = np.argwhere(alpha_mask < 255)
+    print('cropped', cropped)
+
+    print_debug(f"alpha_mask.shape: {alpha_mask.shape}")
+    print_debug(f"cropped.shape:    {cropped.shape}")
+
     res = cv2.matchTemplate(img, tmpl, cv2.TM_CCORR_NORMED, mask=alpha_mask)
+
+    print_debug('res', res)
+
+    #  if (debug):
+        #  plt.figure(figsize=(25, 25))
+        #  plt.imshow(res)
+        #  plt.show()
+
     # Normalize from 0 to 1
     if norm:
+        print_debug(f"normalizing")
         res = cv2.normalize(res, None, 0, 1, cv2.NORM_MINMAX, -1)
-    
+
     match_locations = np.where(res >= threshold)
-    
+
+    print_debug('match_locations', match_locations)
+
     h, w = tmpl.shape[:2]
-    
-    r = []
+
+    rects = []
     for (x, y) in zip(*match_locations[::-1]):
-        r.append(((x, y), (x + w, y + h)))
-        
-    return r
+        r = [ int(x), int(y), w, h ]
+        rects.append(r)
+        # lol do it twice...
+        rects.append(r)
+
+    rects, weights = cv2.groupRectangles(rects, 1, rect_thresh)
+
+    return rects
